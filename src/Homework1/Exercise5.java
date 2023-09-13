0f6e9323440820aa4a6c4712ec2a7a093807e0ea
sers1 <sers156@mail.ru>
2023-09-13 17:03:06 +1000

    hw done

---
+++ src/Homework1/Exercise5.java
@@ -0,0 +1,9 @@
+package Homework1;
+
+public class Exercise5 {public static void main(String[] args) {
+    System.out.println("Part 1");
+    System.out.println("4 * (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11) = " + 4.0 * (1.0 - 1.0 / 3.0 + 1.0 / 5.0 - 1.0 / 7.0 + 1.0 / 9.0 - 1.0 / 11.0));
+    System.out.println("Part 2");
+    System.out.println("4 * (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13) = " + 4.0 * (1.0 - 1.0 / 3.0 + 1.0 / 5.0 - 1.0 / 7.0 + 1.0 / 9.0 - 1.0 / 11.0 + 1.0 / 13.0));
+}
+}
